70aa29d8332721af662b2e1b810c2b2cfc5bfd43
Jose Nison <josenison@users.noreply.github.com>
2026-05-10 03:29:21 +0800

    Add bounding_box_size() convenience method to Shape
    I kept manually computing (max - min) on bounding box results throughout
    my scripts, so added a small helper that returns the dimensions directly
    as a Vector.

--- cadquery/occ_impl/shapes.py
+++ cadquery/occ_impl/shapes.py
@@ -57,6 +57,18 @@ class Shape:
         xmin, ymin, zmin, xmax, ymax, zmax = bbox.Get()
         return Vector(xmin, ymin, zmin), Vector(xmax, ymax, zmax)
 
+    def bounding_box_size(self) -> Vector:
+        """Return the size of the bounding box as a Vector (dx, dy, dz).
+
+        Convenience method I added since I kept computing this manually.
+        """
+        min_pt, max_pt = self.bounding_box()
+        return Vector(
+            max_pt.x - min_pt.x,
+            max_pt.y - min_pt.y,
+            max_pt.z - min_pt.z,
+        )
+
     def fuse(self, other: "Shape") -> "Shape":
         """Boolean union of this shape with another."""
         fuse_op = BRepAlgoAPI_Fuse(self._shape, other._shape)
@@ -91,80 +103,4 @@ class Shape:
         """Return the center of mass of the shape."""
         props = GProp_GProps()
         brepgprop_VolumeProperties(self._shape, props)
-        com = props.CentreOfMass()
-        return Vector(com.X(), com.Y(), com.Z())
-
-    def __repr__(self) -> str:
-        return f"<Shape: {type(self._shape).__name__}>"
-
-
-class Solid(Shape):
-    """Represents a solid 3D shape."""
-
-    @classmethod
-    def make_box(
-        cls,
-        length: float,
-        width: float,
-        height: float,
-        origin: Optional[Vector] = None,
-    ) -> "Solid":
-        """Create a box solid with given dimensions.
-
-        Args:
-            length: Dimension along X axis.
-            width: Dimension along Y axis.
-            height: Dimension along Z axis.
-            origin: Optional origin point (default is (0, 0, 0)).
-        """
-        if origin is None:
-            pnt = gp_Pnt(0.0, 0.0, 0.0)
-        else:
-            pnt = gp_Pnt(origin.x, origin.y, origin.z)
-        builder = BRepPrimAPI_MakeBox(pnt, length, width, height)
-        return cls(builder.Shape())
-
-    @classmethod
-    def make_sphere(cls, radius: float, center: Optional[Vector] = None) -> "Solid":
-        """Create a sphere solid.
-
-        Args:
-            radius: Radius of the sphere.
-            center: Optional center point (default is (0, 0, 0)).
-        """
-        if center is None:
-            builder = BRepPrimAPI_MakeSphere(radius)
-        else:
-            pnt = gp_Pnt(center.x, center.y, center.z)
-            builder = BRepPrimAPI_MakeSphere(pnt, radius)
-        return cls(builder.Shape())
-
-    @classmethod
-    def make_cylinder(
-        cls,
-        radius: float,
-        height: float,
-        origin: Optional[Vector] = None,
-        direction: Optional[Vector] = None,
-    ) -> "Solid":
-        """Create a cylinder solid.
-
-        Args:
-            radius: Radius of the cylinder.
-            height: Height of the cylinder.
-            origin: Optional base center point (default is (0, 0, 0)).
-            direction: Optional axis direction (default is Z axis).
-        """
-        if origin is None:
-            pnt = gp_Pnt(0.0, 0.0, 0.0)
-        else:
-            pnt = gp_Pnt(origin.x, origin.y, origin.z)
-
-        if direction is None:
-            d = gp_Dir(0.0, 0.0, 1.0)
-        else:
-            d = gp_Dir(direction.x, direction.y, direction.z)
-
-        axis = gp_Ax2(pnt, d)
-        builder = BRepPrimAPI_MakeCylinder(axis, radius, height)
-        return cls(builder.Shape())
+        com = props
